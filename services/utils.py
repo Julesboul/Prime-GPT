a1f25f2effce01c988b5054ecf45ca056d46cf4b
Jules Boulet <julesboulet2000@gmail.com>
2024-03-29 11:29:15 +0100

    fix(app): use tesseract with heroku

--- services/utils.py
+++ services/utils.py
@@ -67,7 +67,7 @@ class UtilsService:
 
         text = ""
 
-        path_to_tesseract = r"C:/Program Files/Tesseract-OCR/tesseract.exe" # Path to the tesseract app
+        path_to_tesseract = r"/app/.apt/usr/bin/tesseract" # Path to the tesseract app
 
         pytesseract.tesseract_cmd = path_to_tesseract 
 
